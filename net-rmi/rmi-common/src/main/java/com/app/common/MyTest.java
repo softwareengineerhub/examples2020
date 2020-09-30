2369a863e645d63f8ef7cc559293e8e84bae6caa
Mik <telm.ms@gmail.com>
2020-09-30 17:22:43 +0300

    rmi example correction

--- net-rmi/rmi-common/src/main/java/com/app/common/MyTest.java
+++
@@ -1,9 +0,0 @@
-package com.app.common;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MyTest extends Remote {
-
-    public void doAction() throws RemoteException;
-}
--- net-rmi/rmi-server/src/main/java/com/app/myrmi/server/ServerMain.java
+++ net-rmi/rmi-server/src/main/java/com/app/myrmi/server/ServerMain.java
@@ -1,22 +1,34 @@
 package com.app.myrmi.server;
 
-import java.rmi.RemoteException;
+import com.app.common.BusinessProcessor;
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class ServerMain {
+public class ServerMain implements BusinessProcessor {
 
     public static void main(String[] args) throws Exception {
 
+        ServerMain server = new ServerMain();
+        BusinessProcessor bp = (BusinessProcessor) UnicastRemoteObject.exportObject(server, 0);
+
         Registry registry = LocateRegistry.createRegistry(1099);
-        //registry.rebind("businessProcessor", new MyBusinessProcessorImpl());
-        registry.rebind("//127.0.0.1/myTest", new MyTestImpl());
+        registry.bind("myTest", bp);
 
-
-        //registry.rebind("//127.0.0.1/MyPersonServer", new MyPersonServiceImpl());
-        //Naming.rebind("//127.0.0.1/MyPersonServer", new MyPersonServiceImpl());
         System.out.println("Server ready");
         Thread.sleep(Integer.MAX_VALUE);
     }
 
+    @Override
+    public int add(int a, int b) {
+        System.out.printf("add(%s, %s)%n", a, b);
+        return a + b;
+    }
+
+    @Override
+    public int subtraction(int a, int b) {
+        System.out.printf("sub(%s, %s)%n", a, b);
+        return a - b;
+    }
 }
