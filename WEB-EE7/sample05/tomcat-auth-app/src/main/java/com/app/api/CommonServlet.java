155be0e008bdff69012bd949ffc75ae851427af5
softwareengineerhub <denis.prokopiuk@yahoo.com>
2020-10-09 08:09:23 +0300

    Commit

---
+++ WEB-EE7/sample05/tomcat-auth-app/src/main/java/com/app/api/CommonServlet.java
@@ -0,0 +1,27 @@
+package com.app.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "commonServlet", urlPatterns = {"/commonServlet"})
+public class CommonServlet extends HttpServlet {
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getRemoteUser();
+        String login2 = request.getUserPrincipal().getName();
+
+        PrintWriter pw = response.getWriter();
+        pw.println("<body>");
+        pw.println("COMMON PAGE");
+        pw.println("<br>");
+        pw.println("login="+login);
+        pw.println("<br>");
+        pw.println("login2="+login2);
+        pw.println("</body>");
+    }
+}
